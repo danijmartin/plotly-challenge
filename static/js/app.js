12e445cf8c0f4bd3bf6de898c3258241044577ce
Danielle Martin <danielle.jaclyn.martin@gmail.com>
2021-06-29 05:45:32 -0700

    added gauge and updated bar chart color

--- static/js/app.js
+++ static/js/app.js
@@ -8,6 +8,7 @@ function init() {
         var otu_ids = sample.otu_ids;
         var otu_labels = sample.otu_labels;
         var metadata = data.metadata[0];
+        var wfreq = metadata.wfreq
         // console.log(otu_ids); - code verification
 
         // Populate drop down
@@ -37,7 +38,8 @@ function init() {
         orientation: "h",
         x: topS_values,
         y: stringOtu_ids,
-        text: topOtu_labels
+        text: topOtu_labels,
+        marker: {color: ["#EAF2F8", "#D4E6F1", "#A9CCE3", "#7FB3D5", "#5499C7", "#2980B9", "#2471A3", "#1F618D", "#1A5276", "#154360"]}
         }]
 
         Plotly.newPlot("bar", data);
@@ -66,6 +68,41 @@ function init() {
         // console.log(metadata) - code verification
         Object.entries(metadata).forEach(([key,value]) =>
             d3.select("#sample-metadata").append("p").attr("style", "font-weight: bold").text(`${key}: ${value}`));
+
+        //Gauge Chart
+        var data = [
+            {
+              type: "indicator",
+              mode: "gauge",
+              value: wfreq,
+              title: { text: "Scrubs per Week", font: { size: 24 } },
+              gauge: {
+                axis: { 
+                    range: [0, 9], 
+                    tickmode: "linear",
+                    tick0: 0,
+                    dtick: 1,
+                    tickwidth: 2
+                },
+                bar: { color: "#D2B4DE"},
+                borderwidth: 2,
+                bordercolor: "gray",
+                steps: [
+                  {range: [0, 1], color: "#E8F6F3"},
+                  {range: [1, 2], color: "#D0ECE7"},
+                  {range: [2, 3], color: "#A2D9CE"},
+                  {range: [3, 4], color: "#73C6B6"},
+                  {range: [4, 5], color: "#45B39D"},
+                  {range: [5, 6], color: "#16A085"},
+                  {range: [6, 7], color: "#138D75"},
+                  {range: [7, 8], color: "#117A65"},
+                  {range: [8, 9], color: "#0E6655"}
+                ]
+              }
+            }
+          ];
+          
+          Plotly.newPlot("gauge", data);
     });
 };
 
@@ -84,6 +121,7 @@ function optionChanged() {
         var otu_ids = sample.otu_ids;
         var otu_labels = sample.otu_labels;
         var metadata = data.metadata[`${selection}`];
+        var wfreq = metadata.wfreq
         console.log(sample);
 
         // Rebuild Bar chart data
@@ -118,9 +156,10 @@ function optionChanged() {
         Object.entries(metadata).forEach(([key,value]) =>
             d3.select("#sample-metadata").append("p").attr("style", "font-weight: bold").text(`${key}: ${value}`));
 
-    });
+        // Restyle Gauge Chart
+        Plotly.restyle("gauge", "value", [wfreq]);
 
-    
+    });  
 };
 
 init();
