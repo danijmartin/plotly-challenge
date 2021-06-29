9961bc15be45f8024941854373c3757d4b004f28
Danielle Martin <danielle.jaclyn.martin@gmail.com>
2021-06-29 01:47:18 -0700

    added function to build bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,47 @@
-function init() {
 // Read in data file
+d3.json("/data/samples.json").then(function(data) {
+        
+    // Grab values for dropdown
+    var ids = data.names;
+
+    // Populate drop down
+    var counter = 0;
+    ids.forEach(function(id) {
+    d3.select("#selDataset").append("option").attr("value", `${counter}`).text(id);
+    counter +=1;
+    })
+});
+
+
+
+d3.select("#selDataset").on("change", updatePlots);
+
+function updatePlots() {
+    var selection = d3.select("selDataset").node().value;
+    console.log(selection);
+
     d3.json("/data/samples.json").then(function(data) {
-            
-        // Grab values for dropdown
-        var ids = data.names;
         var sample_id = data.samples.id;
         var sample_values = data.samples.sample_values;
         var otu_ids = data.samples.otu_ids;
         var otu_labels = data.samples.otu_labels;
         var metadata = data.metadata;
 
-        console.log(ids);
-
-        // Populate drop down
-        ids.forEach(function(id) {
-            d3.select("#selDataset").append("option").attr("value", `${id}`).text(id);
-        })
+    barChart();
+    bubbleChart();
+    demographics();
+    guageChart();
     });
 };
 
-init()
+function barChart() {
+    var data = [{
+        type: "bar",
+        orientation: "h",
+        x: sample_values,
+        y: otu_ids,
+        text: otu_labels
+    }]
+
+    Plotly.newPlot("bar", data)
+}
