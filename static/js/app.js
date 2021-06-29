852ab6736aab0109cd104e9ea8f36553877e459e
Danielle Martin <danielle.jaclyn.martin@gmail.com>
2021-06-29 04:40:10 -0700

    completed optionChanged function

--- static/js/app.js
+++ static/js/app.js
@@ -72,6 +72,54 @@ function init() {
 
 //Build event listener
 function optionChanged() {
+    // Assign value to variable (value is id's index inside samples list)
+    var selection = d3.select("#selDataset").property("value");
+    console.log(selection);
+
+    // Read in data file again
+    d3.json("/data/samples.json").then(function(data) {
+        // Grab values for updated charts
+        var sample = data.samples[`${selection}`];
+        var sample_values = sample.sample_values;
+        var otu_ids = sample.otu_ids;
+        var otu_labels = sample.otu_labels;
+        var metadata = data.metadata[`${selection}`];
+        console.log(sample);
+
+        // Rebuild Bar chart data
+        // Limit data to top 10 and reverse order
+        let topS_values = sample_values.slice(0,10).reverse();
+        let topOtu_ids = otu_ids.slice(0,10).reverse();
+        let topOtu_labels = otu_labels.slice(0,10).reverse();
+
+        // Add OTU to OTU ids
+        let stringOtu_ids = []
+        topOtu_ids.forEach(function(num) {
+            stringOtu_ids.push(`OTU ${num}`);
+        });
+
+        // Restyle bar chart
+        Plotly.restyle("bar", "x", [topS_values]);
+        Plotly.restyle("bar", "y", [stringOtu_ids]);
+        Plotly.restyle("bar", "text", [topOtu_labels]);
+
+        //Restyle Bubble Chart
+        Plotly.restyle("bubble", "x", [otu_ids]);
+        Plotly.restyle("bubble", "y", [sample_values]);
+        Plotly.restyle("bubble", "text", [otu_labels]);
+        Plotly.restyle("bubble", "marker.color", [otu_ids]);
+        Plotly.restyle("bubble", "marker.size", [sample_values]);
+
+        // Update Demographics
+        //Clear previous data
+        d3.select("#sample-metadata").selectAll("p").remove();
+
+        // Add new data
+        Object.entries(metadata).forEach(([key,value]) =>
+            d3.select("#sample-metadata").append("p").attr("style", "font-weight: bold").text(`${key}: ${value}`));
+
+    });
+
     
 };
 
